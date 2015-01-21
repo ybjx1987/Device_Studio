0e00919d7afc0f783d90adaf6267afe2bbb317d5
ybjx1987 <yubing@yubingdeMacBook-Air.local>
2015-01-21 22:46:41 +0800

    更新了xmlnode类的说明文件
    完成了XmlNode类的说明文件，在这个文件中说明了XmlNode类的主要功能和接口。

---
+++ Device_Studio/libs/corelib/qdata.cpp
@@ -0,0 +1,21 @@
+#include "qdata.h"
+
+QDataInfo::QDataInfo():
+    m_name("data"),
+    m_group("data"),
+    m_index(-1)
+{
+
+}
+
+QData::QData():
+    QAbstractHost(NULL)
+{
+
+}
+
+QData::~QData()
+{
+
+}
+
