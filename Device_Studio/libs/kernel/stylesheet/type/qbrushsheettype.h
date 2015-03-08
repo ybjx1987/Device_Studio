9d7d46b38ffe21ccd4b2b8d67328b67330bfcbea
ybjx1987 <yubing@yubingdeMacBook-Air.local>
2015-03-08 18:34:57 +0800

    增加了brush编辑器，以及brush的stylesheet

--- Device_Studio/libs/kernel/stylesheet/type/qbrushsheettype.h
+++ Device_Studio/libs/kernel/stylesheet/type/qbrushsheettype.h
@@ -3,12 +3,32 @@
 
 #include "qabstractsheettype.h"
 
+#include <QGradient>
+
+class XmlNode;
+
 class QBrushSheetType : public QAbstractSheetType
 {
     Q_OBJECT
 public:
-    QBrushSheetType();
+    Q_INVOKABLE QBrushSheetType();
     ~QBrushSheetType();
+
+    QString getValueText();
+    QIcon getValueIcon();
+
+    QString getStyleSheet();
+
+
+    virtual bool    toXml(XmlNode * xml);
+    virtual bool    fromXml(XmlNode * xml);
+
+    virtual bool    equal(const QVariant & value);
+protected:
+    QStringList styleSheetParameters(const QGradient &gradient);
+    QStringList styleSheetStops(const QGradient &gradient);
+    QString styleSheetFillName(const QGradient &gradient);
+    QString styleSheetCode(const QGradient &gradient);
 };
 
 #endif // QBRUSHSHEETTYPE_H
